2fa4960af580b8bf88f65e67a19349bbbf3e1459
Yuri Marcon <yuri.erik.oliveira@gmail.com>
2023-05-12 14:18:51 -0300

    Criando arquivo de intalação base para i3wm

---
+++ interface_i3/install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt install \
+    git \
+    xorg \
+    neovim \
+    arandr \
+    lightdm \
+    lightdm-gtk-greeter \
+    pacmanfm \
+    i3 \
+    chromium \
+    neofetch
